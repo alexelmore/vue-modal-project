ac4a53a31d0071f34309f803fc7151a46a06d543
aelmore49 <alexelmore49@gmail.com>
2022-03-04 12:19:41 -0600

    Completion of basic Vue Modal Project

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,26 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>{{ title }}</h1>
+	<p>Welcome!</p>
+	<teleport to=".modals" v-if="showModal">
+		<Modal :theme="theme" @close="toggleModal">
+			<h1 v-if="isDeal">{{ headerDeals }}</h1>
+			<h2 v-if="isDeal">{{ textDeals }}</h2>
+			<h1 v-if="!isDeal">{{ headerFeatures }}</h1>
+			<h2 v-if="!isDeal">{{ textFeatures }}</h2>
+			<template v-if="isDeal" v-slot:links>
+				<div class="tags">
+					<a href="#">Sign Up Now</a>
+					<a href="#">More Info</a>
+				</div>
+			</template>
+			<template v-if="!isDeal" v-slot:features>
+				<div class="tags">
+					<a href="#">Visit Vue</a>
+					<a href="#">Read the Docs</a>
+				</div>
+			</template>
+		</Modal>
+	</teleport>
+	<button class="btn" @click="toggleModal">Show Me the Deals</button>
+	<button class="btn" @click="toggleFeaturesModal">
+		Learn About New Features
+	</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		Modal,
+	},
+	data() {
+		return {
+			title: "Vue All The Things!",
+			headerDeals: "Sign Up NOW!",
+			textDeals: "And Receive all the DEALS!",
+			headerFeatures: "Vue 3 is HERE!",
+			textFeatures: "Start using Vue 3 Today!",
+			theme: "sale",
+			showModal: false,
+			isDeal: false,
+		};
+	},
+	methods: {
+		toggleModal() {
+			this.showModal = !this.showModal;
+			if (!this.isDeal) {
+				this.isDeal = true;
+			}
+		},
+		toggleFeaturesModal() {
+			this.showModal = !this.showModal;
+			if (this.isDeal) {
+				this.isDeal = false;
+			}
+		},
+	},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app,
+.modals {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+.btn {
+	display: block;
+	width: fit-content;
+	font-size: 1.5em;
+	color: #fff;
+	padding: 8px 20px;
+	border-radius: 4px;
+	background-color: rgb(65, 184, 131);
+	cursor: pointer;
+	text-decoration: none;
+	margin: 20px auto;
+	border: 3px solid rgb(52, 73, 94);
+}
+.tags {
+	display: flex;
+	flex-direction: column;
+}
+.tags a {
+	text-decoration: none;
+	color: #2c3e50;
+	font-size: 1em;
+	font-weight: bold;
+}
+.tags a:hover {
+	color: rgb(65, 184, 131);
 }
 </style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="backdrop" @click.self="closeModal">
+		<div class="modal" :class="{ sale: theme === 'sale' ? 'sale' : '' }">
+			<slot>Default Modal Content</slot>
+			<div class="actions">
+				<slot name="links"></slot>
+				<slot name="features"></slot>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	props: ["header", "text", "theme"],
+	methods: {
+		closeModal() {
+			this.$emit("close");
+		},
+	},
+};
+</script>
+
+<style>
+.modal {
+	width: 400px;
+	padding: 20px;
+	margin: 100px auto;
+	background: white;
+	border-radius: 10px;
+}
+.backdrop {
+	top: 0;
+	position: fixed;
+	background: rgba(0, 0, 0, 0.5);
+	width: 100%;
+	height: 100%;
+}
+.modal h1 {
+	color: #03cfb4;
+	border: none;
+	padding: 0;
+}
+.sale {
+	background: #03cfb4;
+	color: rgb(52, 73, 94);
+	border: 2px solid rgb(52, 73, 94);
+}
+
+.sale h1 {
+	color: rgb(52, 73, 94);
+}
+.sale .tags a:hover {
+	color: white;
+}
+</style>
